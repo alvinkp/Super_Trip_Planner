7b51ce7bac3a774e8d692f981edda1dcacf4b743
alvinkp <102767990+alvinkp@users.noreply.github.com>
2022-07-10 00:19:32 -0400

    Updated HTML, CSS & JS
    *HTML*
    
    - Added <header> & <main>
    
    - Added searchContainer to store input and button elements
    
    - Added searchResultContainer to contain cityCurrentWeather & cityFiveDay
    
    - Added Bootstrap to html
    
    *CSS*
    
    - Added modifications to Jumbotron class
    
    *JS*
    
    - Added mySearchButton to contain search button element
    
    - Added myCity to contain user submitted city
    
    - Added Event Listener to mySearchButton
    
    - Added Event Listener to myCity if the user presses Enter instead of clicking on the Search Button
    
    - Added buttonPressed() to call CallAPI();
    
    - Added CallAPI() to initiate API call to One Call Weather API

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,15 +1,34 @@
 var myWeatherApiContainer = document.querySelector("#weatherSection");
+var mySearchButton = document.querySelector(".btn");
+var myCity = document.querySelector("#cityName");
 
-fetch("http://api.openweathermap.org/data/2.5/forecast?q=Uniondale&appid=e1739b9a89959eb08d85a9a92023d8d4", {
+function buttonPressed(){
+    console.log("Pressed!");
+    callAPI();
+}
+
+
+function callAPI(){
+fetch("https://api.openweathermap.org/data/2.5/weather?q=" + myCity.value + "&units=imperial&appid=e1739b9a89959eb08d85a9a92023d8d4", {
 })
   .then(function (response) {
     return response.json();
   })
   .then(function (data) {
     console.log(data);
-    for(var i = 0; i < data.list.length; i++){
     var myAPIResult = document.createElement("p");
-    myAPIResult.textContent = data.list[i].weather[0].description;
+    myAPIResult.textContent = data.main.temp;
     myWeatherApiContainer.appendChild(myAPIResult);
+});
 }
-  });
+
+// If user enters input and presses "ENTER" call the click function on mySearchButton
+myCity.addEventListener("keypress", function(event) {
+  
+    if (event.key === "Enter") {
+      event.preventDefault();
+      mySearchButton.click();
+    }
+})
+
+mySearchButton.addEventListener('click', buttonPressed);
