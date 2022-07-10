a9827ebde5b52a222b34e0fae34434c961a5d1bd
alvinkp <102767990+alvinkp@users.noreply.github.com>
2022-07-10 16:38:56 -0400

    Updated HTML, CSS & JS
    *HTML*
    
    - Added Bootstrap classes to sections for styling
    
    *CSS*
    
    - Overrode the H1 styling by setting font-size to 2rem
    
    *JS*
    
    - Removed ButtonPressed()
    
    - mySearchButton calls CallAPI() instead
    
    - Added event listener to myCity input element so user can just press 'Enter' to submit their city.
    
    - Rewrote callAPI() to fetch from the Geocoding API first and pass the resulting coordinates to the OneCall API that then calls addInfoToHTML() to populate information to HTML
    
    - Added addInfoToHTML() which takes in information and an html Element to create a <p> element and add it the supplied container with the information provided inside.

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -2,33 +2,56 @@ var myWeatherApiContainer = document.querySelector("#weatherSection");
 var mySearchButton = document.querySelector(".btn");
 var myCity = document.querySelector("#cityName");
 
-function buttonPressed(){
-    console.log("Pressed!");
-    callAPI();
+// Add supplied information to an html element
+function addInfoToHTML(info, place) {
+  var myElement = document.createElement("p");
+  myElement.textContent = info;
+  place.appendChild(myElement);
 }
 
-
-function callAPI(){
-fetch("https://api.openweathermap.org/data/2.5/weather?q=" + myCity.value + "&units=imperial&appid=e1739b9a89959eb08d85a9a92023d8d4", {
-})
-  .then(function (response) {
-    return response.json();
+// Get the supplied city's coordinates and then get the current weather 
+function callAPI() {
+  fetch("http://api.openweathermap.org/geo/1.0/direct?q=" + myCity.value + "&limit=1&appid=e1739b9a89959eb08d85a9a92023d8d4", {
   })
-  .then(function (data) {
-    console.log(data);
-    var myAPIResult = document.createElement("p");
-    myAPIResult.textContent = data.main.temp;
-    myWeatherApiContainer.appendChild(myAPIResult);
-});
+    // Take response from fetch and parse to JSON
+    .then(function (response) {
+      return response.json();
+    })
+    // Take parsed response in as data and extract lat and lon, store it and return coordinates object
+    .then(function (data) {
+      console.log(data);
+      var coordinates = {
+        lat: data[0].lat,
+        lon: data[0].lon
+      }
+      return coordinates;
+    })
+
+    // get coordinates from previous .then function and pass it into this new fetch request then create our weather information and add it to our html
+    .then(function (getWeather) {
+      fetch("https://api.openweathermap.org/data/2.5/onecall?lat=" + getWeather.lat + "&lon=" + getWeather.lon + "&units=Imperial&appid=e1739b9a89959eb08d85a9a92023d8d4", {
+      })
+        .then(function (response) {
+          response.json()
+            .then(function (data) {
+              addInfoToHTML("Temp: " + data.current.temp + "\xB0F", myWeatherApiContainer);
+              addInfoToHTML("Wind Speed: " + data.current.wind_speed + "MPH", myWeatherApiContainer);
+              addInfoToHTML("Humidity: " + data.current.humidity, myWeatherApiContainer);
+              addInfoToHTML("UV Index: " + data.current.uvi, myWeatherApiContainer);
+            })
+        })
+    })
 }
+
 
 // If user enters input and presses "ENTER" call the click function on mySearchButton
-myCity.addEventListener("keypress", function(event) {
-  
-    if (event.key === "Enter") {
-      event.preventDefault();
-      mySearchButton.click();
-    }
+myCity.addEventListener("keypress", function (event) {
+
+  if (event.key === "Enter") {
+    event.preventDefault();
+    mySearchButton.click();
+  }
 })
 
-mySearchButton.addEventListener('click', buttonPressed);
+// If user clicks Search button then execute callAPI()
+mySearchButton.addEventListener('click', callAPI);
